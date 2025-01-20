dc66f7efea518911969b478e3b1d261a221cc47d
Neil Sen Easow <neilseneasow@gmail.com>
2025-01-20 21:47:57 +0530

    income_tax

--- income_tax.py
+++ income_tax.py
@@ -1,12 +1,16 @@
 income = int(input("Enter your income: "))
 tax_amount = 0
 
-if income <= 250000:
+if income <= 300000:
     tax_amount = 0
-elif income <= 250000 and income >= 500000:
+elif income > 300000 and income <= 600000:
     tax_amount = income * 0.05
-elif income <= 500000 and income >= 1000000:
+elif income > 600000 and income <= 900000:
     tax_amount = income * 0.10
+elif income > 900000 and income <= 1200000:
+    tax_amount = income * 0.15
+elif income > 1200000 and income <= 1500000:
+    tax_amount = income * 0.20
 else:
     tax_amount = income * 0.30
 

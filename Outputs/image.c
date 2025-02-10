d9285db5bd9935e4d7c2e27cc886e80e6ac635fe
Neil <neilseneasow@gmail.com>
2025-02-10 23:01:21 +0530

    Add output folder

---
+++ Outputs/image.c
@@ -0,0 +1,7 @@
+// This is a simple image processing program that takes an image and applies a filter to it.
+// The filter is a simple box blur.
+// The program is written in C.
+// The program is written in C.
+// The program is written in C.
+// The program is written in C.
+// The program is written in C.
